735238ed6b1c013188d75c937139fd774bd6d6d5
nterraza <nicolas.vega-terrazas@axelspringer.com>
2023-12-22 11:49:11 +0100

    Make it clear which with and height are needed

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -4,38 +4,31 @@ import { useCallback } from "react";
 import Image from "next/image";
 
 const baseURL = "https://d34ftjh07pyt8l.cloudfront.net/";
-const imageNames = [
-  "P3200214_com.jpg",
-  "warsaw_bridge.jpg",
-  "dominika_face.jpg",
-  "P3200433_com.jpg",
-  "P3200220_com.jpg",
-  "julia_bridge.jpg",
-  "dominika_color.jpg",
-  "oslo_people.jpg",
-  "water_jump.jpg",
-  "IMG_4421_com.jpg",
-  "dominica_monum.jpg",
-  "P3200373_com.jpg",
-  "marocco_clif.jpg",
-  "weronika_vog.jpg",
-  "weronika_leaf.jpg",
-  "P1350018 3 18.41.49_com.jpg",
-  "P3200302_com.jpg",
-  "dominika_fontain.jpg",
-  "P3200141_com.jpg",
-  "P3200452-3_com.jpg",
-  "P3200211_com.jpg",
-  "mexican_temple.jpg",
-  "dominika_color_up.jpg",
+const photos = [
+  { src: `${baseURL}P3200214_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}warsaw_bridge.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}dominika_face.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200433_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200220_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}julia_bridge.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}dominika_color.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}oslo_people.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}water_jump.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}IMG_4421_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}dominica_monum.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200373_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}marocco_clif.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}weronika_vog.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}weronika_leaf.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P1350018 3 18.41.49_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200302_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}dominika_fontain.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200141_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200452-3_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}P3200211_com.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}mexican_temple.jpg`, width: 800, height: 600 },
+  { src: `${baseURL}dominika_color_up.jpg`, width: 800, height: 600 },
 ];
-
-const photos = imageNames.map((name) => ({
-  src: `${baseURL}${name}`,
-  width: 800, // default width, adjust as needed
-  height: 600, // default height, adjust as needed
-}));
-
 export default function GalleryPage() {
   const renderPhoto = useCallback(
     ({ imageProps: { alt, style, ...rest } }: RenderPhotoProps) => (
@@ -50,8 +43,8 @@ export default function GalleryPage() {
         {...rest}
         placeholder={undefined}
         objectFit="cover"
-        height={600}
-        width={800}
+        height={parseFloat(style?.height?.toString() ?? "600")}
+        width={parseFloat(style?.width?.toString() ?? "800")}
       />
     ),
     [],
